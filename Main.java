4f053fbf5dadc4b26cb757ad759884f707351bf7
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-08 16:18:48 +0000

    finished practice Q3 Oct 8

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+ import java.util.Scanner;
 /**
- *
- * @author 
+ * This program puts 10 numbers into an array and finds which one is the largest
+ * @author Zach Ramirez 
  */
 public class Main {
 
@@ -9,7 +10,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in); 
+
+    //ask user user for 10 integers 
+    System.out.println("Please enter 10 integers to put into array"); 
+     
+
+    //create an array with 10 spots 
+    int[] integers = new int[10]; 
+
+    //putting their numbers into array using a loop  
+    for(int i = 0; i < 10; i++){
+      integers[i] = input.nextInt(); 
+    }     
+
+     int biggest = integers[0]; 
+
+  
+    //use loop to find out what the largest number is 
+    for(int i = 0; i < 10; i++){
+      if(integers[i] > biggest){
+        biggest = integers[i]; 
+      }
+    } 
+    //tell user what the biggest number is
+     System.out.println("The largest number is " + biggest);
     
   }
 }
